53a6387f8f4008ca334ef34654d13b3a3419dd4e
Dhyan V. <107373897+FlmnD@users.noreply.github.com>
2025-12-18 22:38:51 -0600

    Refactor TreeNode and DFS logic in day7.cpp

--- 2025/day7.cpp
+++ 2025/day7.cpp
@@ -5,70 +5,123 @@
 #include <algorithm>
 #include <unordered_set>
 #include <memory>
+#include <queue>
+#include <stack>
+#include <set>
+#include <utility>
 
 using namespace std;
 
-template <typename T>
-void print2DVec(const vector<vector<T>> &vec)
-{
-    for_each(vec.begin(), vec.end(), [](auto vectemp)
-             {
+typedef long long ll;
 
-                 for_each(vectemp.begin(), vectemp.end(), [](T num)
-                          { cout << num << " "; });
-                 cout << "\n"; });
-}
+// template <typename T>
+// void print2DVec(const vector<vector<T>> &vec)
+// {
+//     for_each(vec.begin(), vec.end(), [](auto vectemp)
+//              {
 
-class TreeNode
-{
-public:
-    TreeNode(int data) : data(data), left(nullptr), right(nullptr) {}
-    TreeNode(int data, std::unique_ptr<TreeNode> &&left, std::unique_ptr<TreeNode> &&right) : data(data), left(std::move(left)), right(std::move(right)) {}
+//                  for_each(vectemp.begin(), vectemp.end(), [](T num)
+//                           { cout << num << " "; });
+//                  cout << "\n"; });
+// }
 
-    void setData(int data)
-    {
-        this->data = data;
-    }
+// template <typename T>
+// struct TreeNode
+// {
+//     T data;
+//     TreeNode<T> *left;
+//     TreeNode<T> *right;
 
-    void setLeft(std::unique_ptr<TreeNode> left)
-    {
-        this->left = std::move(left);
-    }
+//     TreeNode(T val) : data(val), left(nullptr), right(nullptr) {}
 
-    void setRight(std::unique_ptr<TreeNode> right)
-    {
-        this->right = std::move(right);
-    }
+//     static void freeTree(TreeNode<T> **node);
+//     static void printTree(const TreeNode<T> *node);
+// };
 
-    int getData() const
-    {
-        return data;
-    }
+// template <typename T>
+// void TreeNode<T>::freeTree(TreeNode<T> **node)
+// {
+//     stack<TreeNode<T> *> s;
 
-    const TreeNode *getRight() const
-    {
-        return right.get();
-    }
+//     if (!node || !*node)
+//         return;
 
-    const TreeNode *getLeft() const
-    {
-        return left.get();
-    }
+//     s.push(*node);
 
-private:
-    int data;
-    std::unique_ptr<TreeNode> left;
-    std::unique_ptr<TreeNode> right;
-};
+//     while (!s.empty())
+//     {
+//         TreeNode<T> *temp = s.top();
+//         s.pop();
+//         if (temp->left)
+//             s.push(temp->left);
+//         if (temp->right)
+//             s.push(temp->right);
 
-int dfs(TreeNode *root) {
+//         delete temp;
+//         temp = nullptr;
+//     }
 
-    if (!root) return 1;
+//     *node = nullptr;
+// }
 
-    
+// template <typename T>
+// void TreeNode<T>::printTree(const TreeNode<T> *node)
+// {
 
-    
-}
+//     if (!node)
+//         return;
+
+//     printTree(node->left);
+//     cout << node->data << " ";
+//     printTree(node->right);
+// }
+
+// void dfs(const TreeNode<pair<size_t, size_t>> *root, vector<pair<size_t, size_t>> &current, set<vector<pair<size_t, size_t>>> &all)
+// {
+
+//     if (!root)
+//         return;
+
+//     current.push_back(root->data);
+
+//     if (!root->left && !root->right)
+//         all.insert(current);
+//     else
+//     {
+//         dfs(root->left, current, all);
+//         dfs(root->right, current, all);
+//     }
+
+//     current.pop_back();
+// }
+
+// TreeNode<pair<size_t, size_t>> *buildTree(const vector<vector<char>> &grid,
+//                                           size_t R, size_t C,
+//                                           size_t r, size_t c)
+// {
+//     if (r < 0 || r >= R || c < 0 || c >= C)
+//         return nullptr;
+
+//     char cell = grid[r][c];
+//     TreeNode<pair<size_t, size_t>> *node = new TreeNode<pair<size_t, size_t>>(make_pair(r, c));
+
+//     if (r == R - 1)
+//         return node;
+
+//     if (cell == 'S' || cell == '.')
+//     {
+//         node->left = buildTree(grid, R, C, r + 1, c);
+//     }
+//     else if (cell == '^')
+//     {
+//         node->left = buildTree(grid, R, C, r + 1, c - 1);
+//         node->right = buildTree(grid, R, C, r + 1, c + 1);
+//     }
+
+//     return node;
+// }
+
+// PART 1
 
 int main()
 {
@@ -79,25 +132,29 @@ int main()
 
     vector<vector<char>> grid;
 
+    size_t srow = 0;
+    size_t scol = 0;
+
     bool firstPass = true;
 
-    size_t spos = 0;
-
     while (getline(file, line))
-    {
-        if (grid.size() == 0)
-            spos = line.find('S');
+    {   
+        if (firstPass) {
+            firstPass = false;
+            scol = line.find('S');
+        }
         grid.emplace_back(vector<char>(line.begin(), line.end()));
     }
 
     unordered_set<size_t> rpos;
 
+
     int splits = 0;
     int timelines = 0;
 
-    TreeNode root(spos);
+    // auto root = buildTree(grid, grid.size(), grid[0].size(), srow, scol);
 
-    for (size_t i = 0; i < grid.size(); i++)
+        for (size_t i = 0; i < grid.size(); i++)
     {
         for (size_t j = 0; j < grid[0].size(); j++)
         {
@@ -114,18 +171,99 @@ int main()
                     rpos.erase(j);
                     splits++;
                     rpos.insert(j + 1);
-                    root.setRight(make_unique<TreeNode>(j+1));
                     rpos.insert(j - 1);
-                    root.setLeft(make_unique<TreeNode>(j - 1));
                 }
             }
         }
     }
 
+    // vector<pair<size_t, size_t>> op;
+    // set<vector<pair<size_t, size_t>>> all;
 
-    print2DVec(grid);
+    // dfs(root, op, all);
+
+    // //TreeNode<pair<size_t, size_t>>::printTree(root);
+    // cout << "\n";
+    // TreeNode<pair<size_t, size_t>>::freeTree(&root);
+
+    // // timelines = dfs(root);
+
+    // print2DVec(grid);
 
     cout << splits << "\n";
+    //cout << all.size() << "\n";
+
+    return 0;
+}
+
+// PART 2
+
+ll dfs(const vector<string> &grid,
+                  int R, int C,
+                  int r, int c)
+{
+    if (r < 0 || r >= R || c < 0 || c >= C)
+        return 0;
+
+    char cell = grid[r][c];
+
+    if (r == R - 1)
+        return 1;
+
+    if (cell == 'S' || cell == '.')
+    {
+        return dfs(grid, R, C, r + 1, c);
+    }
+    else if (cell == '^')
+    {
+        ll leftPaths = dfs(grid, R, C, r, c - 1);
+        ll rightPaths = dfs(grid, R, C, r, c + 1);
+        return leftPaths + rightPaths;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    ifstream file("day7.txt");
+
+    vector<string> grid;
+    string line;
+
+    int srow = -1, scol = -1;
+    int r = 0;
+
+    while (getline(file, line))
+    {
+        if (!line.empty() && line.back() == '\r')
+        {
+            line.pop_back();
+        }
+
+        grid.push_back(line);
+
+        if (srow == -1)
+        {
+            auto pos = line.find('S');
+            if (pos != string::npos)
+            {
+                srow = r;
+                scol = static_cast<int>(pos);
+            }
+        }
+
+        ++r;
+    }
+
+    int R = static_cast<int>(grid.size());
+    int C = (R > 0 ? static_cast<int>(grid[0].size()) : 0);
+
+    ll totalPaths = dfs(grid, R, C, srow, scol);
+
+    cout << totalPaths << "\n";
 
     return 0;
 }
